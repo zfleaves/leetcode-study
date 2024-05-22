91c0e867fe6d47d69be817d37c0f3618f1cdd208
zhangming <1051403128@qq.com>
2024-05-22 22:22:11 +0800

    feat:tij

--- TypeScript/funciton.ts
+++ TypeScript/funciton.ts
@@ -1,102 +1,31 @@
-/*
- * @Date: 2024-05-22 21:55:57
- * @LastEditors: zhangming 1051403128@qq.com
- * @LastEditTime: 2024-05-22 21:58:39
- * @FilePath: \leetcode-study\TypeScript\funciton.ts
- */
-// 定义一个类  
-class Animal {
-    // 属性  
-    name: string;
+// 跟javascript 定义函数十分相似，可以通过funciton 关键字、箭头函数等形式去定义，例如下面一个
+const add = (a: number, b: number) => a + b
 
-    // 构造函数，用于初始化对象的属性  
-    constructor(name: string) {
-        this.name = name;
-    }
+// 当我们没有提供函数实现的情况下，有两种声明函数类型的方式，如下所示
+// 方式一
+type LongHand = {
+    (a: number): number;
+};
 
-    // 方法  
-    speak(): void {
-        console.log(`The animal says something`);
-    }
+// 方式二
+type ShortHand = (a: number) => number;
+
+// 剩余类型
+// 如果剩余参数 rest 是一个由number类型组成的数组，则如下表示：
+const addRest = (a: number, ...rest: number[]) => rest.reduce(((a, b) => a + b), a)
+
+// 函数重载
+// 上边是声明
+// function add (arg1: string, arg2: string): string
+// function add (arg1: number, arg2: number): number
+// 因为我们在下边有具体函数的实现，所以这里并不需要添加 declare 关键字
+
+// 下边是实现
+function addHeavyLoad (arg1: string | number, arg2: string | number) {
+  // 在实现上我们要注意严格判断两个参数的类型是否相等，而不能简单的写一个 arg1 + arg2
+  if (typeof arg1 === 'string' && typeof arg2 === 'string') {
+    return arg1 + arg2
+  } else if (typeof arg1 === 'number' && typeof arg2 === 'number') {
+    return arg1 + arg2
+  }
 }
-
-// 创建一个类的实例（对象）  
-let dog = new Animal('Buddy');
-
-// 访问对象的属性  
-console.log(dog.name); // 输出：Buddy  
-
-// 调用对象的方法  
-dog.speak(); // 输出：The animal says something  
-
-// 你可以定义一个继承自另一个类的类  
-class Dog extends Animal {
-    bark(): void {
-        console.log('Woof woof!');
-    }
-}
-
-// 创建一个 Dog 类的实例  
-let myDog = new Dog('Max');
-
-// 访问继承的属性  
-console.log(myDog.name); // 输出：Max  
-
-// 调用继承的方法  
-myDog.speak(); // 输出：The animal says something（因为 Dog 类没有重写 speak 方法）  
-
-// 调用 Dog 类特有的方法  
-myDog.bark(); // 输出：Woof woof!
-
-// 在 TypeScript 中，类的成员（包括属性、方法、访问器和构造函数）可以使用访问修饰符来定义它们的可见性和可访问性。
-// TypeScript 支持三种访问修饰符：public、private 和 protected。
-
-// 以下是这些修饰符的定义和用法：
-
-// public：这是默认的访问修饰符。如果省略了修饰符，成员就是 public 的。public 成员可以在任何地方被访问，包括在类内部、类的子类中和类的实例上。
-// typescript
-class MyClass1 {
-    public myPublicProperty: string = 'Hello, public!';
-
-    public myPublicMethod() {
-        console.log(this.myPublicProperty);
-    }
-}
-
-const obj1 = new MyClass1();
-console.log(obj1.myPublicProperty); // 输出 "Hello, public!"  
-obj1.myPublicMethod(); // 输出 "Hello, public!"
-// private：private 成员只能在类的内部被访问。它们不能在类的外部或类的子类中访问。
-// typescript
-class MyClass2 {
-    private myPrivateProperty: string = 'Hello, private!';
-
-    public showPrivateProperty() {
-        console.log(this.myPrivateProperty); // 可以在类内部访问  
-    }
-}
-
-const obj2 = new MyClass2();
-// console.log(obj.myPrivateProperty); // 错误：myPrivateProperty 是私有的  
-obj2.showPrivateProperty(); // 可以通过公共方法来间接访问私有属性
-// protected：protected 成员可以在类内部和类的子类中被访问，但不能在类的外部访问。
-// typescript
-class ParentClass {
-    protected myProtectedProperty: string = 'Hello, protected!';
-
-    protected showProtectedProperty() {
-        console.log(this.myProtectedProperty);
-    }
-}
-
-class ChildClass extends ParentClass {
-    public accessProtectedProperty() {
-        console.log(this.myProtectedProperty); // 可以在子类中访问  
-        this.showProtectedProperty(); // 可以在子类中访问  
-    }
-}
-
-const child = new ChildClass();
-// console.log(child.myProtectedProperty); // 错误：myProtectedProperty 是受保护的  
-child.accessProtectedProperty(); // 可以通过子类的方法间接访问受保护的属性和方法
-// 需要注意的是，尽管 TypeScript 提供了访问修饰符来确保类型安全并增强封装性，但这些修饰符在编译为 JavaScript 时会被移除，因为 JavaScript 不支持这些访问修饰符。因此，TypeScript 的访问修饰符主要用于在 TypeScript 编译时提供类型检查和错误提示，而不是在运行时提供真正的访问控制。在 JavaScript 中，你可以访问任何对象的任何属性（除非使用闭包、原型链或其他 JavaScript 特性来模拟私有性）。
--- TypeScript/generic programming.ts
+++ TypeScript/generic programming.ts
@@ -5,12 +5,12 @@
  * @FilePath: \leetcode-study\TypeScript\generic programming.ts
  */
 // 假设我们用一个函数，它可接受一个 number 参数并返回一个number 参数，如下写法：
-function returnItemNumber (para: number): number {
+function returnItemNumber(para: number): number {
     return para
 }
 
 // 如果我们打算接受一个 string 类型，然后再返回 string类型，则如下写法：
-function returnItemString (para: string): string {
+function returnItemString(para: string): string {
     return para
 }
 
@@ -60,3 +60,67 @@ class Stack<T> {
 }
 const stack = new Stack<number>()
 stack.push(3)
+
+
+// 索引类型、约束类型
+// 索引类型 keyof T 把传入的对象的属性类型取出生成一个联合类型，这里的泛型 U 被约束在这个联合类型中，如下所示：
+function getValue<T extends object, U extends keyof T>(obj: T, key: U) {
+    return obj[key] // ok
+}
+// 上述为什么需要使用泛型约束，而不是直接定义第一个参数为 object类型，是因为默认情况 object 指的是{}，
+// 而我们接收的对象是各种各样的，一个泛型来表示传入的对象类型，比如 T extends object
+const a = {
+    name: 'huihui',
+    age: 18
+}
+// getValue(obj: { name: string; age: number; }, key: "name" | "age"): string | number
+// getValue(a,)
+
+function logLength<T extends { length: number }>(arg: T): void {
+    console.log(arg.length);
+}
+
+logLength('hello'); // 输出 5  
+logLength([1, 2, 3]); // 输出 3  
+
+// 下面的调用会导致编译错误，因为 number 类型没有 length 属性  
+// logLength(123); // 编译错误
+
+// 多类型约束
+// 例如如下需要实现两个接口的类型约束
+interface FirstInterface {
+    doSomething(): number
+}
+
+interface SecondInterface {
+    doSomethingElse(): string
+}
+// 可以创建一个接口继承上述两个接口，如下：
+interface ChildInterface extends FirstInterface, SecondInterface {
+
+}
+
+// 正确使用如下
+class Demo<T extends ChildInterface> {
+    private genericProperty: T
+
+    constructor(genericProperty: T) {
+        this.genericProperty = genericProperty
+    }
+    useT() {
+        this.genericProperty.doSomething()
+        this.genericProperty.doSomethingElse()
+    }
+}
+// 交叉类型（Intersection Types） 这可以通过 & 符号来实现
+class Demo1<T extends FirstInterface & SecondInterface> {
+    private genericProperty: T
+
+    constructor(genericProperty: T) {
+        this.genericProperty = genericProperty
+    }
+    useT() {
+        this.genericProperty.doSomething()
+        this.genericProperty.doSomethingElse()
+    }
+}
